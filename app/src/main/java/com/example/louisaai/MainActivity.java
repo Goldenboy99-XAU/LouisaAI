ad4e9b7ceac175310d0d5ac03e07d9052cd81a55
Goldenboy99-XAU <110536745+Goldenboy99-XAU@users.noreply.github.com>
2023-05-02 21:48:01 +0200

    Splash & Chat Commit

--- app/src/main/java/com/example/louisaai/MainActivity.java
+++ app/src/main/java/com/example/louisaai/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.louisaai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler h = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Splash Screen
+        h.postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent i = new Intent(MainActivity.this,Louisa.class);
+                startActivity(i);
+                finish();
+            }
+        },3500);
     }
 }
---
+++ app/src/main/java/com/example/louisaai/Message.java
@@ -0,0 +1,5 @@
+package com.example.louisaai;
+
+public class Message {
+
+}
