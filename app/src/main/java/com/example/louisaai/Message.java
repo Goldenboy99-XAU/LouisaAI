9078534b69313e5b8da87eeb9c062784636a6375
Goldenboy99-XAU <110536745+Goldenboy99-XAU@users.noreply.github.com>
2023-05-03 19:59:44 +0200

    Final Touch

--- app/src/main/java/com/example/louisaai/Message.java
+++ app/src/main/java/com/example/louisaai/Message.java
@@ -1,5 +1,38 @@
 package com.example.louisaai;
 
 public class Message {
+    public static String SENT_BY_ME = "me";
+    public static String SENT_BY_BOT = "bot";
 
+    String message;
+    String sentBy;
+
+    public static String getSentByMe() {
+        return SENT_BY_ME;
+    }
+
+    public static void setSentByMe(String sentByMe) {
+        SENT_BY_ME = sentByMe;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+
+    public Message(String message, String sentBy) {
+        this.message = message;
+        this.sentBy = sentBy;
+    }
 }
